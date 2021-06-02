a16890d3dfd4288e1b3db4bf557eb2360885c9c2
Jonathan Araujo <jonathan.x939@gmail.com>
2021-06-02 11:08:22 -0300

    exemplo simples de for of

---
+++ conceitos_javascript/na_pratica/for_of.js
@@ -0,0 +1,12 @@
+// for...of
+
+let nome = 'John'
+let nomes = ['Jonathan', 'Caio', 'Vinicius']
+
+for(let char of nome) {
+  console.log(char) 
+}
+
+for(let nome of nomes) {
+  console.log(nome)
+}
