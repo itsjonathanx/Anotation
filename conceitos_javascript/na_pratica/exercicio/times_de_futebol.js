73ef9372ee8009f956eb6d2518a77e1168403f94
Jonathan Araujo <jonathan.x939@gmail.com>
2021-06-08 15:37:19 -0300

    praticando for...of

---
+++ conceitos_javascript/na_pratica/exercicio/times_de_futebol.js
@@ -0,0 +1,428 @@
+const championsLeague = [
+  {
+    country: "Espanha",
+    clubs: [
+      {
+        nameTeam: "Atlético de Madrid",
+        league: "la liga",
+        namePlayers: [
+          {
+            name: "Suárez",
+            number: "9",
+          },
+          {
+            name: "João Félix",
+            number: "7",
+          },
+          {
+            name: "Marcos Llorente",
+            number: "14",
+          },
+        ],
+      },
+      {
+        nameTeam: "Real Madrid",
+        league: "la liga",
+        namePlayers: [
+          {
+            name: "Sergio Ramos",
+            number: "4",
+          },
+          {
+            name: "Benzema",
+            number: "9"
+          },
+          {
+            name: "Marcelo",
+            number: "12"
+          },
+        ],
+      },
+      {
+        nameTeam: "Barcelona",
+        league: "la liga",
+        namePlayers: [
+          {
+            name: "Messi",
+            number: "10",
+          },
+          {
+            name: "Iniesta",
+            number: "8",
+          },
+          {
+            name: "Xavi",
+            number: "6",
+          },
+        ],
+      },
+      {
+        nameTeam: "Sevilla",
+        league: "la liga",
+        namePlayers: [
+          {
+            name: "Papu Gómez",
+            number: "24",
+          },
+          {
+            name: "Jules Koundé",
+            number: "12",
+          },
+          {
+            name: "Youssef En-Nesyri",
+            number: "15",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    country: "Inglaterra",
+    clubs: [
+      {
+        nameTeam: "Manchester City",
+        league: "Premier League",
+        namePlayers: [
+          {
+            name: "Sergio Aguero",
+            number: "10",
+          },
+          {
+            name: "Kevin De Bruyne",
+            number: "17",
+          },
+          {
+            name: "Riyad Mahrez",
+            number: "26",
+          },
+        ],
+      },
+      {
+        nameTeam: "Manchester United",
+        league: "Premier League",
+        namePlayers: [
+          {
+            name: "Paul Pogba",
+            number: "6",
+          },
+          {
+            name: "Bruno Fernandez",
+            number: "18",
+          },
+          {
+            name: "Edinson Cavani",
+            number: "7",
+          },
+        ],
+      },
+      {
+        nameTeam: "Liverpool",
+        league: "Premier League",
+        namePlayers: [
+          {
+            name: "Mohamed Salah",
+            number: "11",
+          },
+          {
+            name: "Virgil van Dijk",
+            number: "4",
+          },
+          {
+            name: "Georginio Wijnaldum",
+            number: "5",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    country: "Alemanha",
+    clubs: [
+      {
+        nameTeam: "Bayer de Munique",
+        league: "Bundesliga",
+        namePlayers: [
+          {
+            name: "Robert Lewandowski",
+            number: "9",
+          },
+          {
+            name: "David Alaba",
+            number: "27",
+          },
+          {
+            name: "Thomas Muller",
+            number: "25"
+          },
+        ],
+      },
+      {
+        nameTeam: "RB Leipzig",
+        league: "Bundesliga",
+        namePlayers: [
+          {
+            name: "Alexander Sorloth",
+            number: "19",
+          },
+          {
+            name: "Dayot Upamecano",
+            number: "5",
+          },
+          {
+            name: "Dominik Marcel Sabitzer",
+            number: "7"
+          },
+        ],
+      },
+      {
+        nameTeam: "Borussia Dortmund",
+        league: "Bundesliga",
+        namePlayers: [
+          {
+            name: "Erling Haaland",
+            number: "9",
+          },
+          {
+            name: "Jadon Sancho",
+            number: "7",
+          },
+          {
+            name: "Marco Reus",
+            number: "11"
+          },
+        ],
+      },
+      {
+        nameTeam: "Wolfsburg",
+        league: "Bundesliga",
+        namePlayers: [
+          {
+            name: "Wout Weghorst",
+            number: "9",
+          },
+          {
+            name: "Maxence Lacroix",
+            number: "4",
+          },
+          {
+            name: "Ridle Baku",
+            number: "20"
+          },
+        ],
+      },
+    ],
+  },
+  {
+    country: "Itália",
+    clubs: [
+      {
+        nameTeam: "Inter de Milão",
+        league: "Série A",
+        namePlayers: [
+          {
+            name: "Lukaku",
+            number: "9"
+          },
+          {
+            name: "Achraf Hakimi",
+            number: "2"
+          },
+          {
+            name: "Lautaro Martínez",
+            number: "10"
+          },
+        ],
+      },
+      {
+        nameTeam: "Milan",
+        league: "Série A",
+        namePlayers: [
+          {
+            name: "Gianluigi Donnarumma",
+            number: "99"
+          },
+          {
+            name: "Zlatan Ibrahimovic",
+            number: "2"
+          },
+          {
+            name: "Hakan Çalhanoglu",
+            number: "10"
+          },
+        ],
+      },
+      {
+        nameTeam: "Atalanta",
+        league: "Série A",
+        namePlayers: [
+          {
+            name: "Luis Muriel",
+            number: "9"
+          },
+          {
+            name: "Duván Zapata",
+            number: "91"
+          },
+          {
+            name: "Robin Gosens",
+            number: "8"
+          },
+        ],
+      },
+      {
+        nameTeam: "Juventus",
+        league: "Série A",
+        namePlayers: [
+          {
+            name: "Cristiano Ronaldo",
+            number: "7"
+          },
+          {
+            name: "Paulo Dybala",
+            number: "10"
+          },
+          {
+            name: "Gianluigi Buffon",
+            number: "77"
+          },
+        ],
+      },
+    ],
+  },
+  {
+    country: "França",
+    clubs: [
+      {
+        nameTeam: "Lille",
+        league: "League 1",
+        namePlayers: [
+          {
+            name: "Burak Yilmaz",
+            number: "17",
+          },
+          {
+            name: "Jonathan David",
+            number: "9",
+          },
+          {
+            name: "Yusuf Yazici",
+            number: "12"
+          },
+        ],
+      },
+      {
+        nameTeam: "Paris Saint-Germain",
+        league: "League 1",
+        namePlayers: [
+          {
+            name: "Neymar",
+            number: "10",
+          },
+          {
+            name: "Kylian Mbappé",
+            number: "7",
+          },
+          {
+            name: "Keylor Navas",
+            number: "1"
+          },
+        ],
+      },
+    ],
+  },
+  {
+    country: "Portugal",
+    clubs: [
+      {
+        nameTeam: "Sporting",
+        league: "Primeira Liga",
+        namePlayers: [
+          {
+            name: "Pedro Gonçalves",
+            number: "28",
+          },
+          {
+            name: "Nuno Alexandre Tavares Mendes",
+            number: "5",
+          },
+          {
+            name: "João Paulo Dias Fernandes",
+            number: "21"
+          },
+        ],
+      },
+      {
+        nameTeam: "Porto",
+        league: "Primeira Liga",
+        namePlayers: [
+          {
+            name: "Mehdi Taremi",
+            number: "9",
+          },
+          {
+            name: "Jesús Corona",
+            number: "17",
+          },
+          {
+            name: "Pepe",
+            number: "3"
+          },
+        ],
+      },
+    ],
+  },
+]
+
+const totalCountriesInCL = championsLeague.length
+console.log(totalCountriesInCL)
+
+for(let country of championsLeague) {
+  console.log(country.country)
+}
+
+function league() {
+  let leagues = [] 
+
+  for(let country of championsLeague) {
+    for(let club of country.clubs) {
+      leagues.push(club.league) 
+    }
+  }
+  
+  leagues = [...new Set(leagues)]
+  for(let league of leagues) {
+    console.log(league)
+  }
+}
+league()
+
+function countCountries() {
+  let clubs = []
+
+  for(let country of championsLeague) {
+    for(let club of country.clubs){
+      console.log(club.nameTeam)
+      if(clubs.indexOf(club.nameTeam) == -1) {
+        clubs.push(club.nameTeam)
+      }
+    }
+  }
+
+  console.log("Total de clubes: ", clubs.length)
+}
+countCountries()
+
+function clubsOfCountry(league) {
+  let clubs = []
+
+  for(let country of championsLeague) {
+    for(let club of country.clubs) {
+      if(club.league === league) {
+        clubs.push(club.nameTeam)
+      }
+    }
+  }
+
+  console.log(`Times do(a) ${league}: ${clubs.join(", ")}`) 
+}
+clubsOfCountry("League 1")
