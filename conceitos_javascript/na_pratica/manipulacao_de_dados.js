064be591c6177da9467fe1387e03233a635fcff7
Jonathan Araujo <jonathan.x939@gmail.com>
2021-06-15 21:20:09 -0300

    refatoração

--- conceitos_javascript/na_pratica/manipulacao_de_dados.js
+++ conceitos_javascript/na_pratica/manipulacao_de_dados.js
@@ -9,8 +9,8 @@ console.log(newPhrase)
 // Separe um texto que contem espaços, em um novo arrays onde cada texto é uma posição do array. Depois disso, transforme o array em um texto e onde eram espaços, coloque _
 let phrase = "Eu quero viver o Amor!"
 let myArray = phrase.split(" ")
-let phraseWithUnderscore = myArray.join("_").toUpperCase()
 console.log(myArray)
+let phraseWithUnderscore = myArray.join("_").toUpperCase()
 console.log(phraseWithUnderscore)
 
 // Manipulando Strings
